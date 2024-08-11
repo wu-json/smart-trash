ab4bdb49e6d8f564985d312c36be9911c3d2bb88
wu-json <jason.wu2153@gmail.com>
2024-08-11 16:07:10 -0400

    feat: update

--- SmartTrash/SmartTrash.ino
+++ SmartTrash/SmartTrash.ino
@@ -2,10 +2,19 @@
 
 Servo servo;
 
+int servoPosition = 0;
+
 void setup() {
   servo.attach(3);
 }
 
 void loop() {
-    servo.write(0);
+    if (servoPosition == 0) {
+        servoPosition = 30;
+    } else {
+        servoPosition = 0;
+    }
+    servo.write(servoPosition);
+    delay(5000);
+    
 }
